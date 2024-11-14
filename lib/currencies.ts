268177d9c873daaddc68f3a45db47efa8e579de4
Prithiviraj <prithiviraj.r2022cse@sece.ac.in>
2024-11-14 18:21:57 +0530

    Add new Currenices

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,10 +1,9 @@
-
-export const Currencies=[
-	{value:"USD", label:"$ Dollar",locale:"en-us"},
-	{value:"EUR", label:"€ Euro",locale:"de-DE"},
-	{value:"JPY", label:"¥ Yen",locale:"ja-Jp"},
-	{value:"GBP", label:"£ Pound",locale:"en-GB"},
+export const Currencies = [
+  { value: "USD", label: "$ Dollar", locale: "en-us" },
+  { value: "EUR", label: "€ Euro", locale: "de-DE" },
+  { value: "JPY", label: "¥ Yen", locale: "ja-Jp" },
+  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+  { value: "INR", label: "₹ Rupee", locale: "en-in" },
 ];
 
-export type Currency=(typeof Currencies)[0]
-
+export type Currency = (typeof Currencies)[0];
